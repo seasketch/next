bc6cceecbe806c1a73513bce07ded41b898a0d88
Chad Burt <chad@underbluewaters.net>
2020-09-16 15:32:28 -0700

    Fix region redraw button on mobile

--- packages/client/src/admin/Settings.tsx
+++ packages/client/src/admin/Settings.tsx
@@ -640,7 +640,7 @@ function MapExtentSettings() {
             <button
               className={`${
                 drawing ? "hidden" : ""
-              } absolute top-28 left-10 cursor-pointer inline-flex items-center px-4 py-2 border border-gray-400 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150`}
+              } sm:absolute sm:top-28 mt-2 sm:mt-0 sm:left-10 cursor-pointer inline-flex items-center px-4 py-2 border border-gray-400 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150`}
               onClick={onRedrawBounds}
             >
               {t("Redraw Bounds")}
