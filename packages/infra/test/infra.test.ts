61607fdc3664faf966e9862f4d0c406ea52ee202
Chad Burt <chad@underbluewaters.net>
2021-04-05 09:05:10 -0700

    Clear tests

---
+++ packages/infra/test/infra.test.ts
@@ -0,0 +1,22 @@
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as Infra from "../lib/ClientStack";
+
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Infra.ClientStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
+});
