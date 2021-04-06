fec254c02328dc4393998af764da32958f94d100
Chad Burt <chad@underbluewaters.net>
2021-04-06 09:38:14 -0700

    ✨ Add link to add new projects from projects list

--- packages/client/src/homepage/ProjectsPage.tsx
+++ packages/client/src/homepage/ProjectsPage.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import Button from "../components/Button";
 import SimpleProjectList from "./SimpleProjectList";
 
 export default function ProjectsPage() {
+  const { t } = useTranslation();
   return (
     <div>
       <SimpleProjectList />
+      <div className="mx-auto max-w-lg">
+        <Button href="/new-project" label={t("Create a Project")} />
+      </div>
     </div>
   );
 }
