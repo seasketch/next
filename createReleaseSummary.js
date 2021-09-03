453130c27ef188eb044af9a9e5f18803c64aa327
Chad Burt <chad@underbluewaters.net>
2021-09-03 09:17:51 -0700

    chore(ci): WIP

---
+++ createReleaseSummary.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+// Must be run from monorepo root
+const changed = require("./changed.json");
+const list = require(".list.json");
+const child_process = require("child_process");
+const fs = require("fs");
+
+console.log(
+  "Approving this pull request will start the production deployment pipeline.\n"
+);
+
+console.log(`
+## Updated Packages
+
+| package name | updated version |
+|--------------|-----------------|`);
+
+for (const package of changed) {
+  const { name, location, version } = package;
+  const updated = list.find((p) => p.name === name);
+  if (!updated) {
+    throw new Error(`Could not find ${name} in list of updated packages`);
+  }
+  console.log(`| ${name} | ${version} → ${updated.version} |`);
+}
+
+const migrations = fs.readFileSync("./migrations.txt").toString();
+const migrationItems = migrations.split("\n");
+if (migrations.length > 1) {
+  console.log(`## 🚨 This change will trigger a database migration\n`);
+  for (const migration of migrationItems) {
+    if (migration.length) {
+      console.log(`  * ${migration}`);
+    }
+  }
+}
+
+console.log("\n## Changelog\n");
+
+console.log(
+  child_process
+    .execSync(`npx conventional-changelog -p angular`)
+    .toString()
+    .split("\n")
+    .slice(3)
+    .join("\n")
+);
