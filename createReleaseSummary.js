23e026bf20bc92ee42881cc20f719270ed589ba1
Chad Burt <chad@underbluewaters.net>
2021-09-05 16:53:29 -0700

    fix(ci): :bug: Quick hotfix CI test

--- createReleaseSummary.js
+++ createReleaseSummary.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-// Must be run from monorepo root
 const changed = require("./changed.json");
 const list = require("./list.json");
 const child_process = require("child_process");
