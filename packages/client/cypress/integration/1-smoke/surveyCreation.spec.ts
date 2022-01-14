f23c127646f0dc01419ac26675a2e69ffd775988
Elana R <eryzna@gmail.com>
2022-01-14 12:03:06 -0500

    user can visit public survey

--- packages/client/cypress/integration/1-smoke/surveyCreation.spec.ts
+++ packages/client/cypress/integration/1-smoke/surveyCreation.spec.ts
@@ -1,21 +1,41 @@
 describe ("Survey creation smoke test", () => {
   describe ("User can visit survey", () => {
-    it ('Visits the homepage', () => {
-      cy.visit('/')
-      cy.contains('SeaSketch')
+    //it ('Visits the homepage', () => {
+    //  cy.visit('/')
+    //  cy.contains('SeaSketch')
+    //})
+    //it ('Logs in the user', () => {
+    //  cy.login('User 1')
+    //  cy.visit('/projects')
+    //  cy.get("#user-menu").should("be.visible");
+    //})
+    //it ('Contains the project', () => {
+    //  cy.contains('Maldives Testing').click()
+    //  cy.contains('Maldives Testing')
+    //}) 
+    beforeEach (() => {
+      cy.intercept("http://localhost:3857/graphql", ((req) => {
+        //console.log(req.body)
+        if (req.body.operationName && req.body.operationName === "CurrentProjectMetadata") {
+          req.alias = "currentProject"
+        }
+      }))
     })
-    it ('Logs in the user', () => {
-      cy.login('User 1')
-      cy.visit('/projects')
-      cy.get("#user-menu").should("be.visible");
-    })
-    it ('Contains the project', () => {
-      cy.contains('Maldives Testing').click()
-      cy.contains('Maldives Testing')
-    }) 
     it ('Can visit the survey', () => {
       cy.visit('/maldivestest/surveys/11/0/practice')
     })
+    it ('Displays the survey start page', () => {
+      cy.wait('@currentProject').its('response').then((resp) => {
+        cy.wrap(resp).then(() => {
+          expect (resp.statusCode).to.eq(200)
+          cy.get('[name="Begin Survey"]')
+          //cy.contains('Begin', {timeout:10000})
+        })
+      })
+
+      //cy.contains('Begin')
+    })
+
     //it ('User can visit project admin', () => {
     //  cy.visit('/maldivestest/admin')
     //})
@@ -23,7 +43,7 @@ describe ("Survey creation smoke test", () => {
   })
 })
 
-//A user can visit homepage
+///A user can visit homepage
 //A user can sign in
 //A user can create a project
 //A user can visit project admin
@@ -32,3 +52,4 @@ describe ("Survey creation smoke test", () => {
 //A user can visit survey admin page
 //A user can modify survey
 //A user can take survey and data is saved appropriately
+//Test on mobile
