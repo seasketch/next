6e7ff0bdcdefff28170472979b1cdd1b124a87b1
Elana R <eryzna@gmail.com>
2022-03-16 12:47:42 -0400

    can draw a polygon refactor

--- packages/client/cypress/integration/1-smoke/surveyCreation.spec.ts
+++ packages/client/cypress/integration/1-smoke/surveyCreation.spec.ts
@@ -497,35 +497,35 @@ describe("Survey creation smoke test", () => {
     //////  })
     //////  cy.get('@nextBtn').click()
     //})
-    //it("Can draw a polygon", () => {
-    //  cy.contains("Fisheries - Commercial, Tuna")
-    //  cy.wait('@basemaps')
-    //  cy.get('.mapboxgl-canvas').each((t) => {
-    //    const canvases = []
-    //    canvases.push(t)
-    //    return canvases
-    //}).then((ary) => {
-    ////////    console.log(ary[0])
-    //    const el = ary[0]
-    //    return el
-    //  }).as('el')
-    //  cy.get('@el').click(300,300)        
-    //    .click(300, 100)
-    //    .click(100, 100)
-    //    .click(100, 300)
-    //    .dblclick(300, 300)
-    //    //.wait(8000)
-    //    //.dblclick(400,200)
-    //  cy.contains('Done').click()
-    //  ////invalid shape
-    //  ////cy.get('.mapboxgl-canvas').click(300, 300)
-    //  ////  .click(100, 600)
-    //  ////  .click(300, 600)
-    //  ////  .click(300, 300)
-    //  ////  .click(400, 400)
-    //  ////  .dblclick(300,300)
-    //  ////cy.get('[name = "Finish Shape"]')
-    //})//
+    it("Can draw a polygon", () => {
+      cy.contains("Fisheries - Commercial, Tuna")
+      cy.wait('@basemaps')
+      cy.get('.mapboxgl-canvas').each((t) => {
+        const canvases = []
+        canvases.push(t)
+        return canvases
+    }).then((ary) => {
+    //////    console.log(ary[0])
+        const el = ary[0]
+        return el
+      }).as('el')
+      cy.get('@el').click(300,300)        
+        .click(300, 100)
+        .click(100, 100)
+        .click(100, 300)
+        .dblclick(300, 300)
+        //.wait(8000)
+        //.dblclick(400,200)
+      cy.contains('Done').click()
+      ////invalid shape
+      ////cy.get('.mapboxgl-canvas').click(300, 300)
+      ////  .click(100, 600)
+      ////  .click(300, 600)
+      ////  .click(300, 300)
+      ////  .click(400, 400)
+      ////  .dblclick(300,300)
+      ////cy.get('[name = "Finish Shape"]')
+    })//
     //it("Can assign attributes to the polygon", () => {
     //  cy.get(".mt-1 > .block").clear()
     //    .type("A great fishing spot for yellowfin tuna.")
