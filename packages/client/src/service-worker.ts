b23d32dafff4e9df2e509dd447663bd0fa776baf
Chad Burt <chad@underbluewaters.net>
2022-06-06 13:47:27 -0700

    Purge stale entries from StaticAssetCache

--- packages/client/src/service-worker.ts
+++ packages/client/src/service-worker.ts
@@ -38,6 +38,10 @@ self.addEventListener("waiting", (event) => {
   );
 });
 
+self.addEventListener("activate", () => {
+  staticAssetCache.purgeStaleEntries();
+});
+
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     // This allows the web app to trigger skipWaiting via
