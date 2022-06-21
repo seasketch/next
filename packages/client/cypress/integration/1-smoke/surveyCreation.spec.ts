e0d8face9c4a4c3813c1d4df014cbe8792550313
Elana R <eryzna@gmail.com>
2022-06-21 16:04:34 -0400

    Remove comments

--- packages/client/cypress/integration/1-smoke/surveyCreation.spec.ts
+++ packages/client/cypress/integration/1-smoke/surveyCreation.spec.ts
@@ -456,17 +456,16 @@ describe("Survey creation smoke test", () => {
             //{$btn.trigger('click')}
           } 
         })
-        
+        cy.get('h4').contains('Fisheries - Commercial, Tuna')
+          .should('exist')
+          .and('be.visible')
+        cy.window().its('mapContext.basemaps').then((maps) => {
+          Object.keys(maps).forEach((key) => {
+            expect (basemaps[maps[key].name]).to.exist
+          })
+        })
         //polygon path for mobile devices
         if (device === "iphone-x") {
-          cy.get('h4').contains('Fisheries - Commercial, Tuna')
-            .should('exist')
-            .and('be.visible')
-          cy.window().its('mapContext.basemaps').then((maps) => {
-            Object.keys(maps).forEach((key) => {
-              expect (basemaps[maps[key].name]).to.exist
-            })
-          })
           cy.get('[data-cy="button-begin"]')
             .should('exist')
             .and('be.visible')
@@ -480,86 +479,85 @@ describe("Survey creation smoke test", () => {
           drawPolygon()
         } 
       })
-      
-      //it(`Can view basemap selector - ${device}`, () => {
-      //  cy.viewport(device)
-      //  cy.get('img').click()
-      //  let values = ['Reset view', 'Focus on location', 'Show scale bar', 'Basemap', 'Maldives Light', 'Satellite']
-      //  values.forEach((val) => {
-      //    cy.get('.fixed > .overflow-y-auto').children().contains(val)
-      //  })
-      //  //satellite basemap thumbnail
-      //  cy.get('img[alt="Satellite basemap"]')
-      //    .should('be.visible')
-      //  cy.get('img[alt="Maldives Light basemap"]')
-      //    .should('be.visible')
-      //})
-      //it (`Can show scale bar - ${device}`, () => {
-      //  cy.viewport(device)
-      //  cy.get('h4').contains('Show scale bar')
-      //  cy.get('[role="switch"]').then(($switch) => {
-      //    {$switch.trigger('click')}
-      //  })
-      //  cy.get('.mapboxgl-ctrl-scale')
-      //    .should('be.visible')
-      //    .as("scaleBar").then((scaleBar) => {
-      //      cy.setLocalStorage("scale bar", scaleBar.html())
-      //      cy.saveLocalStorage()
-      //    })
-      //})
-      //it (`Renders the correct basemap - ${device}`, () => {
-      //  cy.viewport(device)
-      //  cy.contains('Maldives Light').as('maldivesLightBasemap')
-      //  cy.get('@maldivesLightBasemap').then(($btn) => {
-      //    {$btn.trigger('click')}
-      //  })
-      //  cy.get('@maldivesLightBasemap').should('have.class', 'font-semibold')
-      //  cy.contains('Satellite')
-      //    .should('not.have.class', 'font-semibold')
-      //})
-      //it (`Can select different basemap - ${device}`, () => {
-      //  cy.viewport(device)
-      //  cy.contains('Satellite').as('satelliteBasemap')
-      //    cy.get('@satelliteBasemap').then(($btn) => {
-      //      {$btn.trigger('click')}
-      //    })
-      //    .should('have.class', 'font-semibold')   
-      //  cy.contains('Maldives Light')
-      //    .should('not.have.class', 'font-semibold')
-      //})
-      //it(`Shows option to focus on location - ${device}`, () => {
-      //  cy.viewport(device)
-      //  cy.restoreLocalStorage()
-      //  cy.window().its('mapContext.map.transform._center').as('centerCoords').then((center) => {
-      //    cy.setLocalStorage("lat", `${center["lat"]}`)
-      //    cy.setLocalStorage("long", `${center["lng"]}`)
-      //    cy.getLocalStorage("surveyId").then((id) => {
-      //      console.log(id)
-      //      cy.setLocalStorage("surveyId", id)
-      //    });
-      //    cy.saveLocalStorage();
-      //    cy.get('h4').contains('Focus on location').click();
-      //  });
-      //});
-      //it(`Focuses on location - ${device}`, () => {
-      //  cy.viewport(device)
-      //  cy.restoreLocalStorage()
-      //  cy.getLocalStorage('lat').then((lat) => {
-      //    cy.getLocalStorage('long').then((lng) => {
-      //      cy.window().its('mapContext.map.transform._center').then((coords) => {
-      //        expect (coords["lat"]).to.not.equal(lat)
-      //        expect (coords["lng"]).to.not.equal(lng)
-      //      });
-      //    });
-      //  });
-      //  cy.getLocalStorage("scale bar").then((unfocusedScale) => {
-      //    cy.get('.mapboxgl-ctrl-scale')
-      //      .should('be.visible')
-      //      .then((focusedScale) => {
-      //        expect (focusedScale.html()).to.not.equal(unfocusedScale)
-      //    })
-      //  })
-      //});
+      it(`Can view basemap selector - ${device}`, () => {
+        cy.viewport(device)
+        cy.get('img').click()
+        let values = ['Reset view', 'Focus on location', 'Show scale bar', 'Basemap', 'Maldives Light', 'Satellite']
+        values.forEach((val) => {
+          cy.get('.fixed > .overflow-y-auto').children().contains(val)
+        })
+        //satellite basemap thumbnail
+        cy.get('img[alt="Satellite basemap"]')
+          .should('be.visible')
+        cy.get('img[alt="Maldives Light basemap"]')
+          .should('be.visible')
+      })
+      it (`Can show scale bar - ${device}`, () => {
+        cy.viewport(device)
+        cy.get('h4').contains('Show scale bar')
+        cy.get('[role="switch"]').then(($switch) => {
+          {$switch.trigger('click')}
+        })
+        cy.get('.mapboxgl-ctrl-scale')
+          .should('be.visible')
+          .as("scaleBar").then((scaleBar) => {
+            cy.setLocalStorage("scale bar", scaleBar.html())
+            cy.saveLocalStorage()
+          })
+      })
+      it (`Renders the correct basemap - ${device}`, () => {
+        cy.viewport(device)
+        cy.contains('Maldives Light').as('maldivesLightBasemap')
+        cy.get('@maldivesLightBasemap').then(($btn) => {
+          {$btn.trigger('click')}
+        })
+        cy.get('@maldivesLightBasemap').should('have.class', 'font-semibold')
+        cy.contains('Satellite')
+          .should('not.have.class', 'font-semibold')
+      })
+      it (`Can select different basemap - ${device}`, () => {
+        cy.viewport(device)
+        cy.contains('Satellite').as('satelliteBasemap')
+          cy.get('@satelliteBasemap').then(($btn) => {
+            {$btn.trigger('click')}
+          })
+          .should('have.class', 'font-semibold')   
+        cy.contains('Maldives Light')
+          .should('not.have.class', 'font-semibold')
+      })
+      it(`Shows option to focus on location - ${device}`, () => {
+        cy.viewport(device)
+        cy.restoreLocalStorage()
+        cy.window().its('mapContext.map.transform._center').as('centerCoords').then((center) => {
+          cy.setLocalStorage("lat", `${center["lat"]}`)
+          cy.setLocalStorage("long", `${center["lng"]}`)
+          cy.getLocalStorage("surveyId").then((id) => {
+            console.log(id)
+            cy.setLocalStorage("surveyId", id)
+          });
+          cy.saveLocalStorage();
+          cy.get('h4').contains('Focus on location').click();
+        });
+      });
+      it(`Focuses on location - ${device}`, () => {
+        cy.viewport(device)
+        cy.restoreLocalStorage()
+        cy.getLocalStorage('lat').then((lat) => {
+          cy.getLocalStorage('long').then((lng) => {
+            cy.window().its('mapContext.map.transform._center').then((coords) => {
+              expect (coords["lat"]).to.not.equal(lat)
+              expect (coords["lng"]).to.not.equal(lng)
+            });
+          });
+        });
+        cy.getLocalStorage("scale bar").then((unfocusedScale) => {
+          cy.get('.mapboxgl-ctrl-scale')
+            .should('be.visible')
+            .then((focusedScale) => {
+              expect (focusedScale.html()).to.not.equal(unfocusedScale)
+          })
+        })
+      });
       //it(`Renders sector specific attributes - Fisheries - Commercial, Tuna - ${device}`, () => {
       //  cy.viewport(device)
       //  cy.get('img').then((imgs) => {
