1a5427a2a5651a22f956d53ad7057b979c01bc05
Chad Burt <chad@underbluewaters.net>
2022-07-29 10:41:26 -0700

    Show "Offline Support" option in Admin sidebar as soon as enabled

--- packages/client/src/admin/AdminApp.tsx
+++ packages/client/src/admin/AdminApp.tsx
@@ -296,7 +296,11 @@ export default function AdminApp() {
       Container = ({ children }: { children?: ReactNode }) => <>{children}</>;
     }
     return Container;
-  }, [window.location.pathname, data?.project?.name]);
+  }, [
+    window.location.pathname,
+    data?.project?.name,
+    data?.project?.isOfflineEnabled,
+  ]);
 
   if (data && data.project?.sessionIsAdmin === false) {
     return <Redirect to={`/${slug}`} />;
