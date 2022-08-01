fb227ec9adaa177afde4038b66109e59b85cca02
Elana R <eryzna@gmail.com>
2022-08-01 12:47:50 -0800

    Projects on listing page are > 1

--- packages/client/cypress/integration/1-smoke/productionSite.spec.js
+++ packages/client/cypress/integration/1-smoke/productionSite.spec.js
@@ -1,6 +1,22 @@
 describe ('Production site monitoring test', () => {
-  it('Visits the homepage', () => {
-    cy.visit("https://next.seasket.ch/")
-    cy.contains('SeaSketch')
-  })
-})
+  it('Loads the homepage', () => {
+    cy.visit("https://next.seasket.ch/");
+    cy.contains('SeaSketch');
+    cy.get('a').contains('Get started')
+      .should('be.visible');
+  }); 
+  it ('Loads the project page', () => {
+    cy.get('[id = "nav-projects"]')
+      .click();
+    cy.url().should('eq', 'https://next.seasket.ch/projects');
+    //number of projects on listing page is greater than 1
+    let projects = []
+    cy.get('li').each(($li) => {
+      if ($li.parent().attr('href').includes('app')) {
+        projects.push($li.parent().attr('href'))
+      }
+    }).then(() => {
+      expect(projects.length).to.be.gt(1)
+    });
+  });
+});
