28ef6ef109eb831cdcf90551a38416c21be57823
Chad Burt <chad@underbluewaters.net>
2022-08-08 13:47:01 -0700

    Fix redirect url of admin sign out link

--- packages/client/src/admin/AdminApp.tsx
+++ packages/client/src/admin/AdminApp.tsx
@@ -454,7 +454,11 @@ function SidebarContents(props: {
                 </div>
               </div>
               <button
-                onClick={() => logout()}
+                onClick={() =>
+                  logout({
+                    returnTo: window.location.origin,
+                  })
+                }
                 className="group flex items-center px-2 py-2 md:text-sm leading-5 font-medium text-gray-300 rounded-md hover:text-white hover:bg-primary-600 focus:outline-none focus:text-white focus:bg-primary-600 transition ease-in-out duration-75 w-full"
               >
                 <svg
