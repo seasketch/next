a40d156bfc62d45f4419a6898de44859fce55062
Elana R <eryzna@gmail.com>
2022-08-10 09:15:17 -0800

    Simplify survey visit

--- packages/client/cypress/integration/1-smoke/productionSiteMonitoring.spec.js
+++ packages/client/cypress/integration/1-smoke/productionSiteMonitoring.spec.js
@@ -8,31 +8,6 @@ describe ('Production site monitoring test', () => {
     cy.contains('SeaSketch');
     cy.get('a').contains('Get started')
       .should('be.visible');
-    cy.contains('Sign In').click();
-    cy.get('[id="username"]').type("test_user_1@seasketch.org");
-    cy.get('[id="password"]').type("uUGPXmq7iDsh3XA");
-    cy.get('[name="action"]').contains('Continue').click();
-    cy.get('#user-menu').should('be.visible');
-  });
-  after(() => {
-    cy.visit("https://next.seasket.ch/");
-    cy.get('button').then((btn) => {
-      if (btn.text().includes('Sign In')) {
-        cy.get('button').contains('Sign In')
-          .click();
-        cy.get('#user-menu')
-          .should('be.visible')
-          .click();
-        cy.contains('Sign out')
-          .click();
-      } else {
-        cy.get('#user-menu')
-          .should('be.visible')
-          .click();
-        cy.contains('Sign out')
-          .click();
-      }
-    });
   });
   it ('Loads the project page', () => {
     cy.get('[id = "nav-projects"]')
@@ -48,25 +23,12 @@ describe ('Production site monitoring test', () => {
       expect(projects.length).to.be.gt(1)
     });
   });
-  it ('Loads the project', () => {
-    cy.contains('Cypress Production Test Project - cyprod')
-      .should('be.visible')
-      .click();
-    cy.contains('cyprod');
-    cy.contains('Project Administration')
-      .click();
-    cy.contains('Basic Settings');
-  });
-  it ('Loads the survey admin', () => {
-    cy.get('a').contains('Surveys').click({force:true});
-    cy.contains('Cypress Production Test Survey')
-      .click();
-    cy.contains('Cypress Production Test Survey');
-    cy.contains("https://next.seasket.ch/").click()
-  });
-  it('Loads the survey', () => {
+  it ('Loads the survey', () => {
+    cy.visit('https://next.seasket.ch/cyprod/surveys/127/0');
     cy.contains('Welcome to the Survey');
     cy.get('button')
-      .contains('Begin');
-  });
+      .contains('Begin')
+      .click();
+    cy.contains('What is your name?')
+    });
 });
