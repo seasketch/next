8fec9a6c1ae2a05418ce5b3f22b90462cbb937d8
Elana R <eryzna@gmail.com>
2022-09-08 15:16:05 -0800

    Authenticated user visiting invite-only project

--- packages/client/cypress/integration/2-onboarding/userOnboarding.spec.ts
+++ packages/client/cypress/integration/2-onboarding/userOnboarding.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
 import { ProjectAccessControlSetting} from "../../../src/generated/graphql";
 import "cypress-localstorage-commands";
 import {deleteUser} from "../../support/deleteUser.js"
@@ -12,7 +13,7 @@ const slug = generateSlug()
 const users = require("../../fixtures/users.json");
 
 describe ("User onboarding via independent browsing", () => {
-  describe.only ("Unuthenticated user visiting a public project", () => {
+  describe ("Unauthenticated user visiting a public project", () => {
     beforeEach(() => {
       
         cy.window().then((window) => {
@@ -79,8 +80,8 @@ describe ("User onboarding via independent browsing", () => {
         .click(); 
       
       
-      cy.get('a').then((a) => {
-        if (a.text().includes('Sign up')) {
+      //cy.get('a').then((a) => {
+      //  if (a.text().includes('Sign up')) {
           cy.contains('Sign up')
             .click();
           cy.get('#email')
@@ -94,13 +95,16 @@ describe ("User onboarding via independent browsing", () => {
           cy.contains('Authorize App')
           //  .click()
           cy.contains('Accept')
-            .click(); 
-          cy.contains('Join this Project')
-        }else {
-          cy.contains('Sign Out').click()
-        }
-        console.log(a.text())
-      })
+            .click();
+          cy.get('h1').then((header) => {
+            console.log(header)
+          }) 
+          //cy.contains('Join this Project')
+      //  }else {
+      //    cy.contains('Sign Out').click()
+      //  }
+      //  console.log(a.text())
+      //})
       
       
       //cy.contains("Sign up")
@@ -125,47 +129,92 @@ describe ("User onboarding via independent browsing", () => {
     })//////
     
   })  
-  describe('Authenticated user (but not a participant) visiting an invite-only project', () => {
+  describe.only('Authenticated user (but not a participant) visiting an invite-only project', () => {
     beforeEach(() => {
-      cy.intercept('/service-worker.js').as('serviceWorker')
-    })
+      cy.intercept('/service-worker.js').as('serviceWorker'); 
+      //cy.wait('@serviceWorker');
+    });
     before (() => {
       cy.getToken("User 1").then(({ access_token }) => {
         cy.wrap(access_token).as("token");
         cy.setLocalStorage("token", access_token);
         cy.createProject(
-          `Cypress Private Project`,
-          'cy-private',
+          `Cypress Invite-Only Project`,
+          'cy-invite-only',
           ProjectAccessControlSetting.InviteOnly,
           true
-        )
-        .then((projectId) => { 
-          
+        ); 
+      });
+      //cy.login('Authenticated User')
+    });
+    after(() => {
+      cy.deleteProject('cy-invite-only')
+    });
+    it ('Visits the project homepage', () => {
+      Cypress.on('uncaught:exception', (err, runnable) => {
+        // we expect a 3rd party library error with message 'list not defined'
+        // and don't want to fail the test so we return false
+        if (err.message.includes('ServiceWorker')) {
+          return false
         }
-        )
-    })
-  })
-  after(() => {
-    cy.deleteProject('cy-private')
-  })
-  it ('Visits the project homepage', () => {
-    Cypress.on('uncaught:exception', (err, runnable) => {
-      // we expect a 3rd party library error with message 'list not defined'
-      // and don't want to fail the test so we return false
-      if (err.message.includes('ServiceWorker')) {
-        return false
-      }
-      // we still want to ensure there are no other unexpected
-      // errors, so we let them fail the test
-    })
-    cy.visit('/')
-      //cy.contains('Maldives Testing').click()
-      cy.get('a#nav-projects').click()
-
-      cy.wait('@serviceWorker')
-  })
-  it ('Is prompted to request access to private project', () => {
-    //cy.contains('Cypress').click()
-  })
-  })
-})
+        // we still want to ensure there are no other unexpected
+        // errors, so we let them fail the test
+      });
+      cy.visit('/');
+      cy.get('a#nav-projects').click();
+      cy.wait('@serviceWorker');
+    });
+    it ('Can sign in', () => {
+      Cypress.on('uncaught:exception', (err, runnable) => {
+        // we expect a 3rd party library error with message 'list not defined'
+        // and don't want to fail the test so we return false
+        if (err.message.includes('ServiceWorker')) {
+          return false
+        }
+        // we still want to ensure there are no other unexpected
+        // errors, so we let them fail the test
+      });
+      cy.contains('Cypress').click();
+      cy.get('button').contains('Sign in').click();
+      cy.get('#username').type('authenticated@seasketch.org');
+      cy.get('#password').type('password');
+      cy.contains('Continue').click();
+      //cy.get('[title="Sign In"]').click();
+      //cy.get('#username').type('authenticated@seasketch.org');
+      //cy.get('#password').type('password');
+      //cy.contains('Continue').click();
+    });
+    it ('Can share profile', () => {
+      cy.get('button').contains('Share Profile').should('be.visible').as('shareProfile');
+      cy.get('@shareProfile').then((btn) => {
+        {btn.trigger('click')}
+      });
+      cy.contains('User Profile').should('be.visible');
+      cy.get('[name="fullname"]').clear().type('Authenticated Jane');
+      cy.get('[name="fullname"]').should("have.value", "Authenticated Jane"); 
+      cy.get('[data-cy="button-send-request"]').click();
+      cy.contains('Your request').should('be.visible');
+      //cy.login('Authenticated User')
+    });
+    it ('Cannot visit project if request is outstanding', () => {
+      cy.visit('/projects');
+      cy.contains("Cypress").click();
+      //cy.contains('Request').should('not.be.visible')
+      cy.get('h3').then((btn) => {
+        console.log(btn.text())
+        cy.wait(500)
+        //if(btn.text().includes('Sign in')) {
+          cy.get('button').contains('Sign in').click();
+          cy.get('#username').type('authenticated@seasketch.org');
+          cy.get('#password').type('password');
+          cy.contains('Continue').click();
+          cy.contains('Request Awaiting Approval')
+        //} else {
+        //  cy.contains('Request awaiting approval')
+        //}
+        //console.log(btn.text())
+      })
+      
+    });
+  });
+});
