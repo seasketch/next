a0cb67ef03dd4643b8312aaf81825bf2dd9bf74d
Chad Burt <chad@underbluewaters.net>
2022-09-12 11:12:23 -0700

    Distribute sql.js wasm in build rather than referencing cdn

--- packages/client/craco.config.js
+++ packages/client/craco.config.js
@@ -35,6 +35,10 @@ module.exports = {
               },
             ],
           },
+          {
+            test: /\.wasm$/,
+            type: "javascript/auto",
+          },
         ],
       },
     },
