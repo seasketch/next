e7db00f45907e9a12c05c7095cec343b3f095dc1
Elana R <eryzna@gmail.com>
2022-12-02 15:33:53 -0500

    Add cy.log for access_token

--- packages/client/cypress/integration/0-commands/auth0ApiCommands.spec.ts
+++ packages/client/cypress/integration/0-commands/auth0ApiCommands.spec.ts
@@ -4,6 +4,12 @@ import { getAuth0ApiToken, getAuth0CypressUserByEmail, deleteAuth0CypressUser, d
 describe ("Calls to Auth0 management api", () => {
   it('Retrieves correct token', () => {
     getAuth0ApiToken().then((resp) => {
+      console.log(resp)
+      if (resp.access_token !== null) {
+        cy.log('Access token present')
+      } else {
+        cy.log('Access token is null')
+      }
       expect(resp).to.haveOwnProperty('access_token')
     })
   })
