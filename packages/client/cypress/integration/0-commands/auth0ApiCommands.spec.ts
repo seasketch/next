d15f242d0f2e9537d6842556aa765e366eef36ee
Elana R <eryzna@gmail.com>
2022-12-02 16:41:39 -0500

    Update Percy job command; add deleteUsers test

--- packages/client/cypress/integration/0-commands/auth0ApiCommands.spec.ts
+++ packages/client/cypress/integration/0-commands/auth0ApiCommands.spec.ts
@@ -1,6 +1,6 @@
 
 import { getAuth0ApiToken, getAuth0UserByEmail, deleteAuth0CypressUser, deleteAllAuth0CypressUsers, generateSlug, bypassUncaughtException } from '../../support/utils.js'
-
+const axios = require("axios").default;
 describe ("Calls to Auth0 management api", () => {
   it('Retrieves correct token', () => {
     getAuth0ApiToken().then((resp) => {
@@ -19,7 +19,24 @@ describe ("Calls to Auth0 management api", () => {
       getAuth0UserByEmail('test_user_1@seasketch.org', token).then((resp) => {
         expect(resp[0].email).to.eq('test_user_1@seasketch.org')
       })
-    })
-    
+    });
   });
+  it ('Can delete users', () => {
+    getAuth0ApiToken().then((resp) => {
+      const token = resp.access_token
+      //Can only delete 50 users at a time
+      deleteAllAuth0CypressUsers(token)
+      const getAllCypressUsers = {
+        method: 'GET',
+        url: 'https://seasketch.auth0.com/api/v2/users',
+        params: {q: 'email:cypress*', search_engine: 'v3'},
+        headers: {authorization: `Bearer ${token}`, 
+        }
+      };
+      axios.request(getAllCypressUsers).then(resp => {
+        const users = resp.data
+        expect(users.length).to.eq(0)
+      });
+    })
+  })
 })
