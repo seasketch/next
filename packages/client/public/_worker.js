4d6ac368110893c7d393d547efad1abf6c3f003f
Chad Burt <chad@underbluewaters.net>
2023-04-19 10:29:55 -0700

    Fix sprites on Cloudflare Pages

--- packages/client/public/_worker.js
+++ packages/client/public/_worker.js
@@ -7,6 +7,10 @@ const worker = {
       const response = await fetch(spriteUrl);
       const newResponse = new Response(response.body, response);
       newResponse.headers.set("Cache-Control", "public, max-age=500");
+      newResponse.headers.set("Access-Control-Allow-Origin", "*");
+      newResponse.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
+      newResponse.headers.set("Access-Control-Allow-Headers", "Content-Type");
+      newResponse.headers.set("Access-Control-Max-Age", "600");
       return newResponse;
     } else {
       throw new Error("Unrecognized request: " + request.url);
