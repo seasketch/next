3aa3e948745c6c3fa00622c2919fa1397d131d4d
Chad Burt <chad@underbluewaters.net>
2023-05-03 10:18:58 -0700

    Bookmark sprite debugging

--- packages/api/tasks/createBookmarkScreenshot.ts
+++ packages/api/tasks/createBookmarkScreenshot.ts
@@ -1,5 +1,5 @@
 import { Helpers } from "graphile-worker";
-import puppeteer, { Browser, ScreenshotOptions } from "puppeteer";
+import { ScreenshotOptions } from "puppeteer";
 import { sign } from "../src/auth/jwks";
 import { withTimeout } from "../src/withTimeout";
 import * as Sentry from "@sentry/node";
@@ -16,35 +16,13 @@ const ISSUER = (process.env.ISSUER || "seasketch.org")
   .split(",")
   .map((issuer) => issuer.trim());
 
-const HOST =
-  process.env.NODE_ENV !== "development" && process.env.ISSUER
-    ? ISSUER[0]
-    : "http://localhost:3857";
-
-const CLOUDFLARE_IMAGES_TOKEN = process.env.CLOUDFLARE_IMAGES_TOKEN;
-
-// const _browser = puppeteer.launch({
-//   headless: true,
-//   defaultViewport: {
-//     deviceScaleFactor: 2,
-//     width: 1280,
-//     height: 1024,
-//   },
-// });
-
-function getBrowser() {
-  // return _browser;
-  return puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    defaultViewport: {
-      deviceScaleFactor: 2,
-      width: 1280,
-      height: 1024,
-    },
-  });
-}
-
+/**
+ * Run the screenshotter lambda function and store the reference to the
+ * screenshot in the database.
+ *
+ * @param payload {id: number } bookmark id
+ * @param helpers Graphile worker helpers
+ */
 async function createBookmarkScreenshot(
   payload: { id: string },
   helpers: Helpers
@@ -55,6 +33,7 @@ async function createBookmarkScreenshot(
 
   await helpers.withPgClient(async (client) => {
     let span = transaction.startChild({ op: "query bookmark" });
+    // Get bookmark from db
     const { rows } = await client.query(
       `
       select 
@@ -75,14 +54,10 @@ async function createBookmarkScreenshot(
     span.finish();
     span = transaction.startChild({ op: "setup and sign auth token" });
     const bookmark = rows[0];
-    const [width, height] = bookmark.map_dimensions as [number, number];
-    const sidebar = bookmark.sidebar_state as {
-      open: boolean;
-      width: number;
-      isSmall: boolean;
-    };
-
     Sentry.setExtra("bookmark", bookmark);
+
+    // Get the dimensions and clipping details for bookmark
+
     const { project_id, user_id, canonical_email } = bookmark;
     const token = await sign(
       client,
@@ -103,11 +78,23 @@ async function createBookmarkScreenshot(
     }&auth=${token}`;
 
     Sentry.setExtra("url", url);
+    Sentry.setExtra(
+      "debugUrl",
+      url + `&bookmarkUrl=https://api.seasket.ch/bookmarks/${bookmark.id}`
+    );
 
     let clip: undefined | ScreenshotOptions["clip"] = undefined;
 
     span.finish();
+
+    // If the sidebar is open, clip the screenshot to the map area
     span = transaction.startChild({ op: "update bookmark dimensions in db" });
+    const [width, height] = bookmark.map_dimensions as [number, number];
+    const sidebar = bookmark.sidebar_state as {
+      open: boolean;
+      width: number;
+      isSmall: boolean;
+    };
     if (sidebar.open && width >= 1080) {
       clip = {
         x: sidebar.width,
@@ -122,6 +109,7 @@ async function createBookmarkScreenshot(
     }
     span.finish();
 
+    // Get the bookmark style data and sprite images
     const { rows: bookmarkRows } = await client.query(
       `
       SELECT bookmark_data($1) as bookmark
@@ -135,7 +123,10 @@ async function createBookmarkScreenshot(
         `,
       [bookmark.id]
     );
-    const spriteImages = spriteRows[0].sprite_images;
+
+    bookmarkData.spriteImages = spriteRows[0].sprite_images;
+
+    // Fire off request to lambda
     const lambdaPayload = {
       width,
       height,
@@ -156,6 +147,8 @@ async function createBookmarkScreenshot(
       throw new Error("Map screenshot lambda function invocation error");
     }
     span.finish();
+
+    // Parse response from lambda
     let lambdaResponseData: any;
     if (typeof res.Payload === "string") {
       lambdaResponseData = JSON.parse(res.Payload);
@@ -169,6 +162,7 @@ async function createBookmarkScreenshot(
     const body = JSON.parse(lambdaResponseData.body);
 
     span = transaction.startChild({ op: "update image id in db" });
+    // Update bookmark with image id
     await client.query(`update map_bookmarks set image_id = $2 where id = $1`, [
       bookmark.id,
       body.id,
@@ -177,4 +171,6 @@ async function createBookmarkScreenshot(
     transaction.finish();
   });
 }
+
+// Export with timeout
 export default withTimeout(60000, createBookmarkScreenshot);
