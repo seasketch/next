4ce2ce9a0303d6b0a9d40c7778f890f909721a26
Chad Burt <chad@underbluewaters.net>
2023-07-17 10:00:39 -0700

    Update env vars in Uploads handler CDK stack

--- packages/infra/lib/UploadHandlerLambdaStack.ts
+++ packages/infra/lib/UploadHandlerLambdaStack.ts
@@ -25,6 +25,22 @@ export class UploadHandlerLambdaStack extends cdk.Stack {
     }
   ) {
     super(scope, id, props);
+    if (!process.env.UPLOADS_BASE_URL) {
+      throw new Error("UPLOADS_BASE_URL must be set in environment");
+    }
+    if (!process.env.RESOURCES_REMOTE) {
+      throw new Error("RESOURCES_REMOTE must be set in environment");
+    }
+    if (!process.env.TILES_REMOTE) {
+      throw new Error("TILES_REMOTE must be set in environment");
+    }
+    if (!process.env.TILES_BASE_URL) {
+      throw new Error("TILES_BASE_URL must be set in environment");
+    }
+
+    const { UPLOADS_BASE_URL, RESOURCES_REMOTE, TILES_REMOTE, TILES_BASE_URL } =
+      process.env;
+
     const fn = new lambda.DockerImageFunction(this, "SpatialUploadHandler", {
       functionName: "SpatialUploadsHandler",
       vpc: props.vpc,
@@ -47,6 +63,10 @@ export class UploadHandlerLambdaStack extends cdk.Stack {
         R2_SECRET_ACCESS_KEY: process.env.R2_SECRET_ACCESS_KEY!,
         SLACK_TOKEN: process.env.SLACK_TOKEN!,
         SLACK_CHANNEL: process.env.SLACK_CHANNEL!,
+        UPLOADS_BASE_URL,
+        RESOURCES_REMOTE,
+        TILES_REMOTE,
+        TILES_BASE_URL,
       },
       memorySize: 10240,
       reservedConcurrentExecutions: 100,
