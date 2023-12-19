64a2fa772c4b86d74d62c6d25341c6862039fb3d
Chad Burt <chad@underbluewaters.net>
2023-12-19 11:32:16 -0800

    Get rid of warning about div being child of p

--- packages/client/src/components/Spinner.tsx
+++ packages/client/src/components/Spinner.tsx
@@ -9,15 +9,14 @@ export default function Spinner(props: {
   mini?: boolean;
 }) {
   return (
-    <div
+    <span
       style={props.style}
       className={`inline-block inset-y-0 items-center pointer-events-none transition-opacity duration-500 opacity-50 ${props.className}`}
       role="progressbar"
     >
       <svg
-        className={`animate-spin ${
-          props.large ? "h-8 w-8" : props.mini ? "h-3 w-3" : "h-5 w-5"
-        } ${props.color === "white" ? "text-white" : "text-gray-500"}`}
+        className={`animate-spin ${props.large ? "h-8 w-8" : props.mini ? "h-3 w-3" : "h-5 w-5"
+          } ${props.color === "white" ? "text-white" : "text-gray-500"}`}
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
@@ -36,6 +35,6 @@ export default function Spinner(props: {
           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
         ></path>
       </svg>
-    </div>
+    </span>
   );
 }
