ad39b7cbf959202a57b9e7598e3bab082fff2661
Chad Burt <chad@underbluewaters.net>
2024-02-22 09:33:06 -0800

    Add geostats to geojson-inspector

--- packages/geojson-inspector/src/types.ts
+++ packages/geojson-inspector/src/types.ts
@@ -1,24 +1,28 @@
-import { GeostatsLayer } from '../../spatial-uploads-handler/src/geostats';
+import { GeostatsLayer } from "../../spatial-uploads-handler/src/geostats";
 interface BaseInspectorResponse {
-	location: string;
+  location: string;
 }
 
 export interface FailedInspectorResponse extends BaseInspectorResponse {
-	error: string;
-	errorsStatus: number;
+  error: string;
+  errorsStatus: number;
 }
 
 export interface SuccessfulInspectorResponse extends BaseInspectorResponse {
-	contentLength: number;
-	contentType?: string;
-	cacheControl?: string;
-	latency: number;
-	bbox: number[];
-	geostats: GeostatsLayer;
+  contentLength: number;
+  contentType?: string;
+  cacheControl?: string;
+  latency: number;
+  bbox: number[];
+  geostats: GeostatsLayer;
 }
 
-export type InspectorResponse = FailedInspectorResponse | SuccessfulInspectorResponse;
+export type InspectorResponse =
+  | FailedInspectorResponse
+  | SuccessfulInspectorResponse;
 
-export function isSuccessfulInspectorResponse(response: InspectorResponse): response is SuccessfulInspectorResponse {
-	return (response as FailedInspectorResponse).error === undefined;
+export function isSuccessfulInspectorResponse(
+  response: InspectorResponse
+): response is SuccessfulInspectorResponse {
+  return (response as FailedInspectorResponse).error === undefined;
 }
