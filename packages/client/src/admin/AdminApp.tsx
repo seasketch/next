7b609e57c9f9f0635bf50af58bcdf14c7013e59d
Chad Burt <chad@underbluewaters.net>
2024-04-11 16:12:16 -0700

    Restricting Activity tab to superusers for now

--- packages/client/src/admin/AdminApp.tsx
+++ packages/client/src/admin/AdminApp.tsx
@@ -32,6 +32,7 @@ import { GraphqlQueryCacheContext } from "../offline/GraphqlQueryCache/useGraphq
 import LanguageSelector from "../surveys/LanguageSelector";
 import TranslateIcon from "@heroicons/react/outline/TranslateIcon";
 import { useLocalStorage } from "beautiful-react-hooks";
+import useIsSuperuser from "../useIsSuperuser";
 
 const LazyBasicSettings = React.lazy(
   /* webpackChunkName: "AdminSettings" */ () => import("./Settings")
@@ -93,6 +94,8 @@ export default function AdminApp() {
   const history = useHistory();
   const { t } = useTranslation("admin");
 
+  const isSuperuser = useIsSuperuser();
+
   useEffect(() => {
     if (
       data?.project?.sessionParticipationStatus &&
@@ -153,27 +156,31 @@ export default function AdminApp() {
       ),
       path: "/admin",
     },
-    {
-      breadcrumb: t("Activity"),
-      new: true,
-      icon: (
-        <svg
-          className={iconClassName}
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-          />
-        </svg>
-      ),
-      path: "/admin/activity",
-    },
+    ...(isSuperuser
+      ? [
+          {
+            breadcrumb: t("Activity"),
+            new: true,
+            icon: (
+              <svg
+                className={iconClassName}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                />
+              </svg>
+            ),
+            path: "/admin/activity",
+          },
+        ]
+      : []),
     {
       breadcrumb: "Users & Groups",
       icon: (
