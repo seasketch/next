715f51f429e6eb90d154f6e5a7f303f7459b2001
Chad Burt <chad@underbluewaters.net>
2025-03-06 10:17:38 -0800

    Fixes #845

--- packages/client/src/admin/data/BasemapEditor.tsx
+++ packages/client/src/admin/data/BasemapEditor.tsx
@@ -40,93 +40,92 @@ export default function BaseMapEditor() {
   const { confirmDelete } = useDialog();
   return (
     <>
-      <div>
-        <div className="p-4">
-          <div className="mb-4">
-            <Button
-              small
-              label={t("Add a new map")}
-              onClick={() => setAddModalOpen(true)}
-            />
-            <Button
-              className="ml-2"
-              small
-              disabled={!mapContext.selectedBasemap}
-              label={t("Edit")}
-              onClick={() => setEditModalOpen(true)}
-            />
-            <Button
-              className="ml-2"
-              small
-              disabled={
-                !mapContext.selectedBasemap || deleteMutationState.loading
-              }
-              label={t("Delete")}
-              onClick={() => {
-                confirmDelete({
-                  message: t("Are you sure you want to delete this map?"),
-                  description: t("This action cannot be undone."),
-                  onDelete: async () => {
-                    await deleteBasemap({
-                      variables: {
-                        id: parseInt(mapContext.selectedBasemap!),
-                      },
-                      onError,
-                      update: (cache) => {
-                        const id = cache.identify(
-                          data!.projectBySlug!.basemaps!.find(
-                            (b) =>
-                              b.id === parseInt(mapContext.selectedBasemap!)
-                          )!
-                        );
+      <div className="flex flex-col h-full">
+        <div className="p-4 pb-1">
+          <Button
+            small
+            label={t("Add a new map")}
+            onClick={() => setAddModalOpen(true)}
+          />
+          <Button
+            className="ml-2"
+            small
+            disabled={!mapContext.selectedBasemap}
+            label={t("Edit")}
+            onClick={() => setEditModalOpen(true)}
+          />
+          <Button
+            className="ml-2"
+            small
+            disabled={
+              !mapContext.selectedBasemap || deleteMutationState.loading
+            }
+            label={t("Delete")}
+            onClick={() => {
+              confirmDelete({
+                message: t("Are you sure you want to delete this map?"),
+                description: t("This action cannot be undone."),
+                onDelete: async () => {
+                  await deleteBasemap({
+                    variables: {
+                      id: parseInt(mapContext.selectedBasemap!),
+                    },
+                    onError,
+                    update: (cache) => {
+                      const id = cache.identify(
+                        data!.projectBySlug!.basemaps!.find(
+                          (b) => b.id === parseInt(mapContext.selectedBasemap!)
+                        )!
+                      );
 
-                        client.cache.evict({
-                          id,
-                        });
+                      client.cache.evict({
+                        id,
+                      });
 
-                        const projectId = cache.identify(
-                          projectData.data!.projectBySlug!
-                        );
-                        cache.modify({
-                          id: projectId,
-                          fields: {
-                            basemaps(existingBasemapRefs, { readField }) {
-                              return existingBasemapRefs.filter(
-                                // @ts-ignore
-                                (basemapRef) => {
-                                  return (
-                                    mapContext.selectedBasemap !==
-                                    readField("id", basemapRef)
-                                  );
-                                }
-                              );
-                            },
+                      const projectId = cache.identify(
+                        projectData.data!.projectBySlug!
+                      );
+                      cache.modify({
+                        id: projectId,
+                        fields: {
+                          basemaps(existingBasemapRefs, { readField }) {
+                            return existingBasemapRefs.filter(
+                              // @ts-ignore
+                              (basemapRef) => {
+                                return (
+                                  mapContext.selectedBasemap !==
+                                  readField("id", basemapRef)
+                                );
+                              }
+                            );
                           },
-                        });
-                      },
-                    });
-                  },
-                });
-              }}
-            />
-          </div>
-          {addModalOpen && (
-            <CreateBasemapModal
-              onRequestClose={() => setAddModalOpen(false)}
-              onSave={(id) => {
-                mapContext.manager?.setSelectedBasemap(id.toString());
-              }}
-            />
-          )}
-          <BasemapControl basemaps={data?.projectBySlug?.basemaps || []} />
+                        },
+                      });
+                    },
+                  });
+                },
+              });
+            }}
+          />
         </div>
-        {editModalOpen && mapContext.selectedBasemap && (
-          <BasemapEditorPanel
-            onRequestClose={() => setEditModalOpen(false)}
-            basemapId={parseInt(mapContext.selectedBasemap)}
+        {addModalOpen && (
+          <CreateBasemapModal
+            onRequestClose={() => setAddModalOpen(false)}
+            onSave={(id) => {
+              mapContext.manager?.setSelectedBasemap(id.toString());
+            }}
           />
         )}
+        <div className="flex-1 overflow-y-auto">
+          <BasemapControl basemaps={data?.projectBySlug?.basemaps || []} />
+        </div>
       </div>
+      {editModalOpen && mapContext.selectedBasemap && (
+        <BasemapEditorPanel
+          onRequestClose={() => setEditModalOpen(false)}
+          basemapId={parseInt(mapContext.selectedBasemap)}
+        />
+      )}
     </>
   );
 }
