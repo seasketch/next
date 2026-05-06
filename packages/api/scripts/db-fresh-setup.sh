18f41c43141405feaa1961766e4cbacfc562732b
Chad Burt <chad@underbluewaters.net>
2026-05-06 14:55:05 -0700

    Remove smart comment stuff

--- packages/api/scripts/db-fresh-setup.sh
+++ packages/api/scripts/db-fresh-setup.sh
@@ -12,41 +12,6 @@ echo "Applying fresh database setup fixes..."
 docker exec "$CONTAINER_NAME" psql -U "$DB_USER" -d postgres -v ON_ERROR_STOP=1 -c \
   "grant connect on database ${DB_NAME} to graphile, anon, seasketch_user, seasketch_superuser;"
 
-# Reapply smart comments needed for PostGraphile naming stability.
-docker exec "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -v ON_ERROR_STOP=1 -c \
-  "DO \$\$
-  BEGIN
-    IF to_regprocedure('public.delete_offline_tile_package(uuid)') IS NOT NULL THEN
-      EXECUTE 'comment on function public.delete_offline_tile_package(id uuid) is E''@name deleteOfflineTilePackageFn''';
-    END IF;
-    IF to_regprocedure('public.delete_report_tab(integer,integer)') IS NOT NULL THEN
-      EXECUTE 'comment on function public.delete_report_tab(tab_id integer, move_cards_to_tab_id integer) is E''@name deleteReportTabFn''';
-    END IF;
-    IF to_regprocedure('public.update_google_maps_tile_api_session(text,text,text,timestamp with time zone,text,integer,integer,text)') IS NOT NULL THEN
-      EXECUTE 'comment on function public.update_google_maps_tile_api_session(p_map_type text, p_language text, p_region text, p_expires_at timestamp with time zone, p_session text, p_tile_width integer, p_tile_height integer, p_image_format text) is E''@name updateGoogleMapsTileApiSessionFn''';
-    END IF;
-    IF to_regprocedure('public.project_background_jobs_esri_feature_layer_conversion_task(public.project_background_jobs)') IS NOT NULL THEN
-      EXECUTE 'comment on function public.project_background_jobs_esri_feature_layer_conversion_task(job public.project_background_jobs) is E''@fieldName esriFeatureLayerConversionTaskComputed''';
-    END IF;
-  END
-  \$\$;"
-
-docker exec "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -v ON_ERROR_STOP=1 -c \
-  "DO \$\$
-  BEGIN
-    IF EXISTS (
-      SELECT 1
-      FROM pg_constraint c
-      JOIN pg_class t ON t.oid = c.conrelid
-      JOIN pg_namespace n ON n.oid = t.relnamespace
-      WHERE n.nspname = 'public'
-        AND t.relname = 'esri_feature_layer_conversion_tasks'
-        AND c.conname = 'esri_feature_layer_conversion_ta_project_background_job_id_fkey'
-    ) THEN
-      EXECUTE 'comment on constraint esri_feature_layer_conversion_ta_project_background_job_id_fkey on public.esri_feature_layer_conversion_tasks is E''@foreignSingleFieldName esriFeatureLayerConversionTaskByJob''';
-    END IF;
-  END
-  \$\$;"
 
 # Ensure trigger-invoked functions resolve unqualified symbols during COPY.
 docker exec "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -v ON_ERROR_STOP=1 -c \
