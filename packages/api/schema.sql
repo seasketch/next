8f00888fdbdf2a98c513b4c1b49e452e5da1a24e
Chad Burt <chad@underbluewaters.net>
2026-06-02 22:27:12 +0000

    Don't set is_geography_clipping_enabled = true unless new sketch classes are POLYGON

--- packages/api/schema.sql
+++ packages/api/schema.sql
@@ -8791,58 +8791,100 @@ $$;
 CREATE FUNCTION public.create_sketch_class_from_template("projectId" integer, template_sketch_class_id integer) RETURNS public.sketch_classes
     LANGUAGE plpgsql SECURITY DEFINER
     AS $$
-    declare 
-      base sketch_classes;
-      created sketch_classes;
-      num_similarly_named int;
-      new_name text;
-      default_report_id integer;
-      default_clipping_geography_id integer;
-      enable_geography_clipping boolean;
-    begin
-      if session_is_admin("projectId") then
-        select id into default_report_id from reports where project_id = "projectId";
-        if default_report_id is null then
-          select id into default_report_id from create_default_report("projectId"::integer);
-        end if;
-        select id into default_clipping_geography_id from project_geography where project_id = "projectId" order by (
-          case
-            when name like 'EEZ%' then 0
-            when name like 'Exclusive Economic Zone%' then 0
-            when client_template = 'eez' then 1
-            else 2
-          end
-        ) limit 1;
+declare
+  base sketch_classes;
+  created sketch_classes;
+  num_similarly_named int;
+  new_name text;
+  default_report_id integer;
+  default_clipping_geography_id integer;
+  enable_geography_clipping boolean;
+begin
+  if session_is_admin("projectId") then
+    select id into default_report_id from reports where project_id = "projectId";
+    if default_report_id is null then
+      select id into default_report_id from create_default_report("projectId"::integer);
+    end if;
+    select id into default_clipping_geography_id
+    from project_geography
+    where project_id = "projectId"
+    order by (
+      case
+        when name like 'EEZ%' then 0
+        when name like 'Exclusive Economic Zone%' then 0
+        when client_template = 'eez' then 1
+        else 2
+      end
+    )
+    limit 1;
 
-        enable_geography_clipping = default_report_id is not null and default_clipping_geography_id is not null;
-   
-        select * into base from sketch_classes where id = template_sketch_class_id;
-        if base is null then
-          raise exception 'Sketch Class with id=% does not exist', template_sketch_class_id;
-        end if;
-        if base.is_template = false then
-          raise exception 'Sketch Class with id=% is not a template', template_sketch_class_id;
-        end if;
-        -- TODO: add suffix to name if there are duplicates
-        select count(*) into num_similarly_named from sketch_classes where project_id = "projectId" and form_element_id is null and name ~ (base.name || '( \(\d+\))?');
-        new_name = base.name;
-        if num_similarly_named > 0 then
-          new_name = new_name || ' (' || num_similarly_named::text || ')';
-        end if;
-        insert into sketch_classes (project_id, name, geometry_type, allow_multi, geoprocessing_project_url, geoprocessing_client_name, geoprocessing_client_url, mapbox_gl_style, preprocessing_endpoint, preprocessing_project_url, is_geography_clipping_enabled) values ("projectId", new_name, base.geometry_type, base.allow_multi, base.geoprocessing_project_url, base.geoprocessing_client_name, base.geoprocessing_client_url, base.mapbox_gl_style, base.preprocessing_endpoint, base.preprocessing_project_url, enable_geography_clipping) returning * into created;
-        perform initialize_sketch_class_form_from_template(created.id, (select id from forms where sketch_class_id = base.id));
-        if default_report_id is not null then
-          perform set_primary_report_for_sketch_class(created.id, default_report_id);
-        end if;
-        if default_clipping_geography_id is not null then
-          insert into sketch_class_geographies (sketch_class_id, geography_id) values (created.id, default_clipping_geography_id);
-        end if;
-        return created;
-      else
-        raise exception 'Permission denied';
-      end if;
-    end;
-  $$;
+    select * into base from sketch_classes where id = template_sketch_class_id;
+    if base is null then
+      raise exception 'Sketch Class with id=% does not exist', template_sketch_class_id;
+    end if;
+    if base.is_template = false then
+      raise exception 'Sketch Class with id=% is not a template', template_sketch_class_id;
+    end if;
+
+    -- Never enable clipping by default for non-polygon classes.
+    enable_geography_clipping = base.geometry_type = 'POLYGON'
+      and default_report_id is not null
+      and default_clipping_geography_id is not null;
+
+    -- TODO: add suffix to name if there are duplicates
+    select count(*) into num_similarly_named
+    from sketch_classes
+    where project_id = "projectId"
+      and form_element_id is null
+      and name ~ (base.name || '( \(\d+\))?');
+    new_name = base.name;
+    if num_similarly_named > 0 then
+      new_name = new_name || ' (' || num_similarly_named::text || ')';
+    end if;
+
+    insert into sketch_classes (
+      project_id,
+      name,
+      geometry_type,
+      allow_multi,
+      geoprocessing_project_url,
+      geoprocessing_client_name,
+      geoprocessing_client_url,
+      mapbox_gl_style,
+      preprocessing_endpoint,
+      preprocessing_project_url,
+      is_geography_clipping_enabled
+    ) values (
+      "projectId",
+      new_name,
+      base.geometry_type,
+      base.allow_multi,
+      base.geoprocessing_project_url,
+      base.geoprocessing_client_name,
+      base.geoprocessing_client_url,
+      base.mapbox_gl_style,
+      base.preprocessing_endpoint,
+      base.preprocessing_project_url,
+      enable_geography_clipping
+    ) returning * into created;
+
+    perform initialize_sketch_class_form_from_template(
+      created.id,
+      (select id from forms where sketch_class_id = base.id)
+    );
+    if default_report_id is not null then
+      perform set_primary_report_for_sketch_class(created.id, default_report_id);
+    end if;
+    if default_clipping_geography_id is not null then
+      insert into sketch_class_geographies (sketch_class_id, geography_id)
+      values (created.id, default_clipping_geography_id);
+    end if;
+    return created;
+  else
+    raise exception 'Permission denied';
+  end if;
+end;
+$$;
 
 
 --
@@ -11809,8 +11851,15 @@ $$;
 CREATE FUNCTION public.geography_clipping_layers_object_key(g public.geography_clipping_layers) RETURNS text
     LANGUAGE sql STABLE SECURITY DEFINER
     AS $$
-    select remote from data_upload_outputs where type = 'FlatGeobuf' and data_source_id = (
-      select data_source_id from data_layers where id = g.data_layer_id
+  select
+    regexp_replace(
+    regexp_replace(remote, '^[^:]+://[^/]+/', ''), -- Remove protocol and host
+    '^/', '' -- Remove leading slash if present
+    )
+  from data_upload_outputs
+  where type = 'FlatGeobuf'
+    and data_source_id = (
+    select data_source_id from data_layers where id = g.data_layer_id
     );
   $$;
 
